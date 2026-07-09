ad4695554f2925a58fab5d156a19f096684fbda0
Rumiia Kontsiialova <rumiia.kontsiyalova@gmail.com>
2026-07-09 19:02:13 +0200

    Added burger menu component and connected it to HomePage.Added burger menu color variables/styles to index.css

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,18 @@
-function HomePage() {
+import BurgerMenu from "../../components/BurgerMenu/BurgerMenu";
+import { useState } from "react";
+
+export default function HomePage() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div>
+      <BurgerMenu onClick={() => setIsMenuOpen(true)} />
+
+        <p>Меню открыто: {isMenuOpen ? "Да" : "Нет"}</p>
+
       <h1>home</h1>
     </div>
   );
 }
 
-export default HomePage;

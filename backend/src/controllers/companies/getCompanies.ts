d084b664f86dee11e64670298f312497fb8cdfa3
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2026-07-13 07:05:29 +0200

    checkpoint

--- backend/src/controllers/companies/getCompanies.ts
+++ backend/src/controllers/companies/getCompanies.ts
@@ -1,12 +1,46 @@
 import { type Request, type Response } from "express";
 import { getCompaniesRepo } from "../../repositories/index.js";
+import createError from "http-errors";
 
 export const getCompanies = async (req: Request, res: Response) => {
-  const companies = await getCompaniesRepo();
+  const rawPage: number = parseInt(String(req.query.page ?? ""), 10);
+  const rawLimit: number = parseInt(String(req.query.limit ?? ""), 10);
+  const rawCityId = parseInt(String(req.query.cityId), 10);
+  const rawCategoryId = parseInt(String(req.query.categoryId), 10);
+
+  if (isNaN(rawCityId) || rawCityId < 1) {
+    throw createError(400, "Invalid cityId. City ID must be a positive integer.");
+    //throw new Error("Invalid cityId. City ID must be a positive integer.");
+  }
+
+  if (isNaN(rawCategoryId) || rawCategoryId < 1) {
+    throw createError(400, "Invalid categoryId. Category ID must be a positive integer.");
+    //throw new Error("Invalid categoryId. Category ID must be a positive integer.");
+  }
+
+  const page = isNaN(rawPage) || rawPage < 1 ? 1 : rawPage;
+  const cityId = rawCityId;
+  const categoryId = rawCategoryId;
+
+  let limit = isNaN(rawLimit) || rawLimit < 1 ? 10 : rawLimit;
+
+  limit = Math.min(limit, 100);
+
+  const { companies, total } = await getCompaniesRepo({
+    page,
+    limit,
+    filters: { cityId, categoryId },
+  });
 
   res.status(200).json({
     status: "success",
     code: 200,
     data: companies,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    },
   });
 };
--- backend/src/repositories/companies/getCompanies.repo.ts
+++ backend/src/repositories/companies/getCompanies.repo.ts
@@ -1,30 +1,96 @@
-import { Company, Address, Zip, City, State } from "../../db/models/index.js";
+import Sequelize from "sequelize";
+import {
+  Company,
+  Address,
+  Zip,
+  City,
+  State,
+  Company_Category,
+  Category,
+} from "../../db/models/index.js";
 
-export const getCompaniesRepo = async () => {
-  const data: any = await Company.findAll({
-    attributes: ["id", "name", "description", "logo"],
+export const getCompaniesRepo = async ({
+  page,
+  limit,
+  filters,
+}: {
+  page: number;
+  limit: number;
+  filters: any;
+}) => {
+  const offset = (page - 1) * limit;
+  const whereConditions: any = {};
+
+  if (filters.cityId) {
+    whereConditions["$Addresses.aglomerationId$"] = filters.cityId;
+  }
+
+  if (filters.categoryId) {
+    whereConditions["$Categories.id$"] = filters.categoryId;
+  }
+
+  const data = await Company.findAndCountAll({
+    attributes: {
+      include: ["id", "name", "description", "logo"],
+      exclude: ["createdAt", "updatedAt"],
+    },
     include: [
       {
+        model: Category,
+        attributes: ["id", "name", "description"],
+        through: { attributes: [] }, // Exclude junction table attributes
+      },
+      {
         model: Address,
+        attributes: ["street", "houseNr", "additionalAdrsInfo", "latitude", "longitude"],
         include: [
-          {
-            model: Zip,
-            attributes: ["code"],
-          },
-          {
-            model: City,
-            attributes: ["name"],
-          },
-          {
-            model: State,
-            attributes: ["name"],
-          },
+          { model: Zip, attributes: ["code"] },
+          { model: City, attributes: ["name"] },
+          { model: State, attributes: ["name"] },
         ],
       },
     ],
-    raw: true,
-    //nest: false,
+    where: whereConditions,
+    subQuery: false,
+    limit,
+    offset,
+    order: [["id", "ASC"]],
+    distinct: true,
+    nest: true,
   });
 
-  return data;
+  const companies = data.rows.map((company) => {
+    const companyData = company.get({ plain: true });
+
+    const addresses = companyData.Addresses.map((address: any) => {
+      return {
+        street: address.street,
+        houseNr: address.houseNr,
+        additionalAdrsInfo: address.additionalAdrsInfo,
+        zip: address.Zip.code,
+        city: address.City.name,
+        state: address.State.name,
+        coordinates: { latitude: address.latitude, longitude: address.longitude },
+      };
+    });
+
+    const categories = companyData.Categories.map((category: any) => {
+      return {
+        id: category.id,
+        name: category.name,
+        description: category.description,
+      };
+    });
+
+    return {
+      id: companyData.id,
+      name: companyData.name,
+      description: companyData.description,
+      logo: companyData.logo,
+      categories,
+      addresses,
+    };
+  });
+
+  return { companies: companies, total: data.count };
 };

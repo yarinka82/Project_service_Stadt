ca0eeb0f4aa33c37377698cb41ceb26842fb1a76
Nataliia Kozak <kozak.svet@gmail.com>
2026-08-06 18:29:04 +0200

    feat: add CategoryTile, categories constant, Page 3 icons; fix App.css import

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -1,25 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import SearchPage from '../SearchPage/SearchPage';
-import './App.tsx';
+import './App.css';
 import Header from '../../components/Header/Header.tsx';
 import Footer from '../../components/Footer/Footer.tsx';
 import AglomerationsPage from '../AglomerationsPage/AglomerationsPage.tsx';
 
 function App() {
   return (
-    <div className='appWrapper'>
+    <div className="appWrapper">
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-          <Route
-        path="/aglomerations"
-        element={<AglomerationsPage />}
-        />
-      
+        <Route path="/aglomerations" element={<AglomerationsPage />} />
+
         <Route path="/search" element={<SearchPage />} />
         <Route path="*" element="not found" />
       </Routes>
+
       <Footer />
     </div>
   );

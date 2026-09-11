55dfcc595661d43ec35009e5d23eecc4845fb9b2
Nataliia Kozak <kozak.svet@gmail.com>
2026-09-11 02:02:21 +0200

    setup Redux Toolkit core and provider, remove zustand

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './components/App/App.tsx';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
